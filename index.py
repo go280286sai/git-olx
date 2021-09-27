e307de612d494660456413a62c49b67f3e63c78b
Aleksandr <go280286sai@gmail.com>
2021-09-27 20:46:36 +0300

    edit settings

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from datetime import datetime
 import sqlite3
 import kurs
+import settings
 
 m = True
 while m == True:
@@ -17,4 +18,21 @@ while m == True:
     if n == 0:
         m = False
     if n == 4:
-        kurs.kurs()
+        k = True
+
+
+        while k == True:
+            print('1. Загрузить новые курсы валют')
+            print('2. Просмотреть курсы валют')
+            print('0. Выйти в предыдущее меню') 
+            ks = int(input('Введите номер: '))
+            if ks == 1: 
+                kurs.kurs()
+            elif ks == 2:
+                kurs.tab_kurs()
+            elif ks == 0:
+                k = False
+            else:
+                print("Вы не правильно ввели цифру, попробуйте еще")
+    if n == 5:
+        settings.setf()
